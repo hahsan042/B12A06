730297090bdb9ebf11dbd3dfd3fc9050119cd03a
hahsan042 <hahsan042@gmail.com>
2025-09-08 12:35:23 +0600

    all plants function added

--- greenEarth.js
+++ greenEarth.js
@@ -93,4 +93,62 @@ const categoryCard=(Catid)=>{
   })
 
  }
+ const allplants=()=>{
+  fetch("https://openapi.programming-hero.com/api/plants")
+  .then(res=>res.json())
+  .then(data=>{
+    const plantsData=data
+    allplantsShow(plantsData.plants)
+  })
+ }
+const allplantsShow=(plantsFn)=>{
+  plantsFn.forEach(singlePlant=>{
+    console.log(singlePlant)
+    plantContainer.innerHTML +=  ` <div
+              class="w-full md:max-w-xs bg-white rounded-2xl shadow-md overflow-hidden p-4"
+            >
+              <!-- Image -->
+              <div
+                class="w-full h-40 bg-gray-100 rounded-lg flex items-center justify-center"
+              >
+                <img
+                  src="${singlePlant.image}"
+                  alt="${singlePlant.category}"
+                  class="object-cover rounded-lg h-full w-full"
+                />
+              </div>
+
+              <!-- Body -->
+              <div class="mt-4 space-y-2">
+                <!-- Title -->
+                <h1 class="text-sm font-bold text-gray-800">${singlePlant.category}</h1>
+
+                <!-- Description (2 lines only) -->
+                <p class="text-gray-600 text-xs line-clamp-2">
+                  ${singlePlant.description}
+                </p>
+
+                <!-- Category & Price -->
+                <div class="flex justify-between items-center">
+                  <span
+                    class="px-3 py-1 text-xs bg-green-100 text-green-700 font-medium rounded-full"
+                  >
+                   ${singlePlant.name}
+                  </span>
+                  <span class="text-sm font-bold text-gray-700">৳${singlePlant.price}</span>
+                </div>
+
+                <!-- Button -->
+                <button
+                  class="w-full mt-3 py-2 bg-green-700 text-white font-semibold rounded-full hover:bg-green-800 transition"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+    `
+    
+  })
+}
+ allplants()
  loadCategory()
